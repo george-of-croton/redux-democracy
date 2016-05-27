b0fac12123cc006217bcbfcbd247d97a08b49eb3
George Armstrong <George@Georges-MacBook.local>
2016-05-27 14:53:11 +1200

    make it so

--- test/immutable_spec.js
+++ test/immutable_spec.js
@@ -16,11 +16,40 @@ describe( 'immutability', () => {
 				'Trainspotting',
 				'28 Days Later',
 				'Sunshine'
-				));
+			));
 			expect(state).to.equal(List.of(
 				'Trainspotting',
 				'28 Days Later'
-				));
+			));
+		})
+	})
+
+	describe('a tree', () => {
+		function addMovie(currentState, movie) {
+			return currentState.set(
+				'movies',
+				currentState.get('movies').push(movie)
+			);
+		}
+		it('is immutable', () => {
+			let state = Map ({
+				movies: List.of('Trainspotting', '28 Days Later')
+			});
+			let nextState = addMovie(state, 'Sunshine');
+
+			expect(nextState).to.equal(Map({
+				movies: List.of(
+					'Trainspotting',
+					'28 Days Later',
+					'Sunshine'
+				)
+			}));
+			expect(state).to.equal(Map({
+				movies: List.of(
+					'Trainspotting',
+					'28 Days Later'
+				)
+			}))
 		})
 	})
 
