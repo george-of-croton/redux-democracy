bdcaeb5063128a10dee1708350d2f145854d3d57
George Armstrong <George@Georges-MacBook.local>
2016-05-30 14:42:07 +1200

    make it so

--- src/core.js
+++ src/core.js
@@ -1,11 +1,22 @@
-import List from 'immutable';
+import {List, Map} from 'immutable';
 
 export function setEntries(state, entries) {
 	return state.set('entries', List(entries));
 }
 
+function getWinners(vote) {
+	if(!vote) return [];
+	const [a, b] = vote.get('pair');
+	const aVotes = vote.getIn(['tally', a], 0);
+	const bVotes = vote.getIN(['tally', b], 0);
+	if (aVotes > bVotes) 	 return [a];
+	else if (aVote < bVotes) return [b];
+	else 					 return [a, b];
+}
+
 export function next(state) {
-	const entries = state.get('entries');
+	const entries = state.get('entries')
+						 .concat(getWinners(state.get('vote')));
 	return state.merge({
 		vote: Map({pair: entries.take(2)}),
 		entries: entries.skip(2)
